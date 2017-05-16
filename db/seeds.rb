74107a43f424fdfd631ff4ceca9efd0f1db9208a
Quan Nguyen <minhquan.nguyen@citynow.vn>
2017-05-16 15:39:29 +0700

    Updated  data seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,17 @@
 10.times do |index|
-  User.create!(email: "user#{index}@email.com", password: '123123123')
+  unless User.find_by(email: "user#{index}@email.com")
+    User.create!(email: "user#{index}@email.com", password: '123123123')
+  end
 end
+
+5.times do |index|
+  if ChatRoom.find_by(id: index + 1).nil?
+    ChatRoom.create!(name: "ChatRoom#{index+1}")
+  end
+end
+
+unless Bot.find_by(email: 'bot@email.com')
+  Bot.create!(email: "bot@email.com", password: '123123123', user_type: 'bot')
+end
+
 puts "Done"
