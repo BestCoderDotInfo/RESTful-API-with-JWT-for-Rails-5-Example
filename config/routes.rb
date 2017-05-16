116ccbd736f55c40d813d825f9948803da0427f3
Quan Nguyen <minhquan.nguyen@citynow.vn>
2017-05-16 15:38:35 +0700

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :home, only: :index
+  resources :chat_rooms
+  resources :users, only: :new do
+    member do
+      get 'message', to: 'message#show'
+    end
+  end
   scope module: 'api/v1', constraints: ApiVersion.new('v1', true) do
     post 'login' => 'authentication#authenticate'
     resources :users, only: :index
