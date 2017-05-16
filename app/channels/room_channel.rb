86835126dbb82eb5b8c02bab79ec7bd8288186a4
Quan Nguyen <minhquan.nguyen@citynow.vn>
2017-05-16 15:40:59 +0700

    Edit channel

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -11,6 +11,10 @@ class RoomChannel < ApplicationCable::Channel
   def speak(data)
     # ActionCable.server.broadcast "room_channel", comment: data['comment']
     # Comment.create!(body: data['comment'], user_id: User.first.id)
-    Comment.create!(body: data['data']['comment'], user_id: data['data']['uid'].to_i)
+    # Comment.create!(body: data['data']['comment'], user_id: data['data']['uid'].to_i)
+    room = ChatRoom.unscope(:where).find(data['data']['room'].to_i)
+    if room
+      room.comments.create!(body: data['data']['comment'], user_id: data['data']['uid'].to_i)
+    end
   end
 end
